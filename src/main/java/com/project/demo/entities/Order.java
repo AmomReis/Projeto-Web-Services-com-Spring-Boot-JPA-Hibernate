29513d4ccec08536643bb3777bec20a3ce20dd81
Amom Reis <mirandoamom@gmail.com>
2025-07-15 16:24:05 -0300

    OrderStatus enum

--- src/main/java/com/project/demo/entities/Order.java
+++ src/main/java/com/project/demo/entities/Order.java
@@ -1,6 +1,7 @@
 package com.project.demo.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.demo.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -22,12 +23,15 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -45,6 +49,14 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
