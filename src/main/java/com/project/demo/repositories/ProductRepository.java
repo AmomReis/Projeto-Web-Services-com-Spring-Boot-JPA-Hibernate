860d1a8e61a60c32dc87e547f8b8066627c6e045
Amom Reis <mirandoamom@gmail.com>
2025-07-16 17:54:02 -0300

    Product

---
+++ src/main/java/com/project/demo/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.project.demo.repositories;
+
+import com.project.demo.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+}
