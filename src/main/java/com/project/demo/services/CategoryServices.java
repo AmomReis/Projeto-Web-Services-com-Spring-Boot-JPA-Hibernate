3b03c54862ff160eb4e3ed46784b775053c7dc1f
Amom Reis <mirandoamom@gmail.com>
2025-07-16 17:32:44 -0300

    Category

---
+++ src/main/java/com/project/demo/services/CategoryServices.java
@@ -0,0 +1,25 @@
+package com.project.demo.services;
+
+import com.project.demo.entities.Category;
+import com.project.demo.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServices {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
